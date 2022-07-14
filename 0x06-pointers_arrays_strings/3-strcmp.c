4387a224e7835cbd5ca8039eef3b0694d807a4f1
Popsicool <akinolasamson1234@gmail.com>
2022-07-14 15:26:01 +0000

    update files

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,15 +7,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i = 0, b = 0;
+int i;
+int R;
 
-while (b == 0)
+i = 0;
+
+while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
 {
-if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-break;
-b = *(s1 + i) - *(s2 + i);
 i++;
 }
-
-return (b);
+R = s1[i] - s2[i];
+return (R);
 }
