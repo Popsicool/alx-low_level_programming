edd05d24d9ab530ab1178740257e83a1ec4406a2
Popsicool <akinolasamson1234@gmail.com>
2022-07-18 12:59:39 +0000

    update file

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,10 +4,9 @@
 /**
  * print_diagsums - the sum of the two diagonals of a square matrix of integers
  * @a: 2d array of chars
- *@size: number matrix
+ * @size: number matrix
  *Return: void
  */
-
 void print_diagsums(int *a, int size)
 {
 int i;
