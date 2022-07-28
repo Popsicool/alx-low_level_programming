f31874a36c27268bdc54af81bb7d632f11d4badb
Popsicool <akinolasamson1234@gmail.com>
2022-07-28 13:01:53 +0000

    update files

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,20 +14,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *concat;
 
 	if (s1 == NULL)
-		s1 = "";
+	s1 = "";
 	if (s2 == NULL)
-		s2 = "";
+	s2 = "";
 	for (i = 0; s1[i]; i++)
-		len++;
+	len++;
 	concat = malloc(sizeof(char) * (len + 1))
 	if (concat == NULL)
-		return (NULL);
-
+	return (NULL);
 	len = 0;
 	for (i = 0; s1[i]; i++)
-		concat[len++] = s1[i]
+	concat[len++] = s1[i]
 	for (i = 0; s2[i] && i < n; i++)
-		concat[len++] = s2[i]
+	concat[len++] = s2[i]
 	concat[len] = '\0';
 	return (0);
 }
