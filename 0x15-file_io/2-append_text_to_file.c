72f9ea10d8e78ab921d1ca330f634eec85725968
Popsicool <akinolasamson1234@gmail.com>
2022-08-22 16:51:46 +0100

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
         for (i = 0; text_content[i] != '\0'; i++)
             ;
         status = write(file, text_content, i);
-        if (status < 0)
+        if (status == -1)
             return (-1);
     }
     close(file);
