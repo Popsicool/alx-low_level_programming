ef2d09df6307b5a756756c85fa20d0df7648728d
Popsicool <akinolasamson1234@gmail.com>
2022-08-22 16:38:28 +0000

    fixed betty and added advance task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,26 +3,26 @@
  * create_file - a function that creates a file.
  * @filename: the name of the file to create
  * @text_content: a NULL terminated string to write to the file
- * Returns: 1 on success, -1 on failure
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-    int i, file, status;
+	int i, file, status;
 
-    if (filename == NULL)
-        return (-1);
-    file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (file < 0)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file < 0)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i] != '\0'; i++)
-            ;
-        status = write(file, text_content, i);
-        if (status < 0)
-            return (-1);
-    }
-    close(file);
-    return (1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		status = write(file, text_content, i);
+		if (status < 0)
+			return (-1);
+	}
+	close(file);
+	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,21 +12,21 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int i, file, status;
+	int i, file, status;
 
-    if (filename == NULL)
-        return (-1);
-    file =  open(filename, O_WRONLY | O_APPEND);
-    if (file < 0)
-        return (-1);
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i] != '\0'; i++)
-            ;
-        status = write(file, text_content, i);
-        if (status == -1)
-            return (-1);
-    }
-    close(file);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+	file =  open(filename, O_WRONLY | O_APPEND);
+	if (file < 0)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		status = write(file, text_content, i);
+		if (status == -1)
+			return (-1);
+	}
+	close(file);
+	return (1);
 }
