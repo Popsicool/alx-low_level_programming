a0909f9f87ff6197df0dc254daf2c3c5c9ad3def
Popsicool <akinolasamson1234@gmail.com>
2022-10-20 19:44:48 +0000

    task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - a function that adds an element to the hash table.
+ * @ht: he hash table you want to add or update the key/value to
+ * @key: The key
+ * @value: The value pair of the key
+ * Return: 1 on success, 0 if failed
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	char *copy_value;
+	unsigned long int index, i;
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+
+	copy_value = strdup(value);
+	if (copy_value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = copy_value;
+			return (1);
+		}
+
+	}
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+	{
+		free(copy_value);
+		return (0);
+	}
+	new_node->value = copy_value;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
