56aa171863fbb4ae22da96fa23d8510c190e893d
Popsicool <akinolasamson1234@gmail.com>
2023-01-27 17:02:09 +0100

    task 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -17,6 +17,11 @@ int jump_search(int *array, size_t size, int value)
 	size_t start = 0;
 	size_t i;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
 	while (size > jump && array[jump] < value)
 	{
 		printf("Value checked array[%li] = [%i]\n", start, array[start]);
