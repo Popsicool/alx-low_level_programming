63527dc8ce6172cc9cba78060148ad85c60ebb7f
Popsicool <akinolasamson1234@gmail.com>
2023-01-27 15:35:23 +0100

    initial commit for mandatory tasks before betty check

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+
+#include "search_algos.h"
+
+/**
+ * linear_search - function that searches for
+ * a value in an array of integers using the Linear search algorithm
+ * @array: the array of integers ti search
+ * @size: the size of the array
+ * @value: thw value to search for 
+ * Return: the index if found or -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
