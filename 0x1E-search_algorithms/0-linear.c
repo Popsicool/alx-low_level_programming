4e7547b086d146719e4358b704c8c94cb9f3fb4a
Popsicool <akinolasamson1234@gmail.com>
2023-01-27 15:51:16 +0100

    task 0 amended

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,6 +13,10 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
+	if (size == 0)
+	{
+		return (-1);
+	}
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%zu] = [%d]\n", i, array[i]);
